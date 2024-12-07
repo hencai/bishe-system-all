54cc5d719ca45dd0b094c69f9e0eda76e7ee5380
xbai <1411804691@qq.com>
2024-12-07 09:40:49 +0800

    成功添加登陆注册页面的路由配置

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,30 +1,53 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Layout from '../components/Layout/MainLayout';
 import Welcome from '../pages/Welcome';
 import Detection from '../pages/Detection';
 import History from '../pages/History/history'; 
 import Users from '../pages/Users';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const router = createBrowserRouter([
+  {
+    path: '/auth',
+    children: [
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: '',
+        element: <Navigate to="/auth/login" />,
+      },
+    ],
+  },
   {
     path: '/',
     element: <Layout />,
     children: [
       {
-        path: '/',
+        path: 'welcome',
         element: <Welcome />,
       },
       {
-        path: '/detection',
+        path: 'detection',
         element: <Detection />,
       },
       {
-        path: '/history',
+        path: 'history',
         element: <History />,
       },
       {
-        path: '/users',
+        path: 'users',
         element: <Users />,
+      },
+      {
+        path: '',
+        element: <Navigate to="/welcome" />,
       },
     ],
   },
